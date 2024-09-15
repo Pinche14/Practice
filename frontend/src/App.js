87d9cad99b3fdf8a01fe9a51ad293784a6fd0867
Pinche14 <shubham70254@gmail.com>
2024-09-15 22:54:25 +0530

    POST

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,11 @@ import RegisterForm from './components/RegisterForm'
 
 const App = () => {
 
+  const [blogPostData, setBlogPostData] = useState({
+    title:"",
+    body:""
+  })
+
   // const [users, setUsers] = useState([
   //   {
   //     id: 1,
@@ -106,20 +111,96 @@ const App = () => {
   //   console.log(window.innerHeight,window.innerWidth)
   // });
  
-  const [counter, setCounter] = useState(0)
-  const [sync, setSync] = useState(false)
+  // const [counter, setCounter] = useState(0)
+  // const [sync, setSync] = useState(false)
 
-  useEffect(() => {
-    console.log("Rendering")
-    document.title = "React Tut" + counter
-  },[sync])
+  // useEffect(() => {
+  //   console.log("Rendering")
+  //   document.title = "React Tut" + counter
+  // },[sync,counter])
 
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/posts",{
+  //     method:'GET',
+  //   }).then((response) =>{
+  //     return response.json()
+  //   }).then((data) => {
+  //     console.log(data)
+  //   });
+  // })
+
+  // useEffect(() => {
+  //   const controller = new AbortController();
+    
+  //   async function fetchUsers() {
+  //     try{
+  //       const response = await fetch(
+  //         "https://jsonplaceholder.typicode.com/posts",
+  //         {signal: controller.signal}
+  //       );
+  //       const json = await response.json();
+  //       console.log(json)
+  //       console.log(controller.signal)
+
+  //     } catch(err) {
+  //       console.log(err)
+  //     }
+  //   }
+  //   fetchUsers();
+  //   return () => {
+  //      controller.abort();
+  //      console.log(controller.signal)
+  //   }
+  // })
   return (
+
     <div>
-      <div>You clicked the button</div>
-      <button onClick={() => {setCounter((count)=> count+1)}}>Click Me</button>
-      <button onClick={() => setSync((current) => !current)}>Sync</button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (blogPostData.title && blogPostData.body){
+            fetch('https://jsonplaceholder.typicode.com/posts',{
+              method:'POST',
+              body: {
+                userId: 1,
+                title: blogPostData.title,
+                body: blogPostData.body
+              }
+            })
+          }
+        }}
+      >
+      <div>  
+        <label htmlFor='title'>Title </label>
+        <input type="text" id="title" value={blogPostData.title} 
+          onChange={(e) => {
+            setBlogPostData((currentBlogPostData) => ({
+              ...currentBlogPostData,
+              title: e.target.value
+            }))
+          }}
+        />
+      </div>
+      <div>  
+        <label htmlFor='body'>Body </label>
+        <input type="text" id="body" value={blogPostData.body} 
+          onChange={(e) => {
+            setBlogPostData((currentBlogPostData) => ({
+              ...currentBlogPostData,
+              body: e.target.value
+            }))
+          }}
+        />
+      </div>
+      <button>Create Post</button>
+      </form>
+      
     </div>
+    // <div>
+    //   <div>You clicked the button</div>
+    //   <button onClick={() => {setCounter((count)=> count+1)}}>Click Me</button>
+    //   <button onClick={() => {setSync((current) => !current)}}>Sync</button>
+    // </div>
    
     // <div>
     //   <div>
