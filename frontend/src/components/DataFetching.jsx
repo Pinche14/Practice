d7e5b99810d1dcdfe5057a46307ba382ba9f136b
Pinche14 <shubham70254@gmail.com>
2024-10-07 23:46:08 +0530

    fetch

---
+++ frontend/src/components/DataFetching.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DataFetching = () => {
+  return (
+    <div>DataFetching</div>
+  )
+}
+
+export default DataFetching
+
+//https://jsonplaceholder.typicode.com/users/1
