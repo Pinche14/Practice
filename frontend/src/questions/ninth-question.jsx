927adfe75341289dbb336a59fdcf4beeb3ea4b4e
Pinche14 <shubham70254@gmail.com>
2024-10-23 23:31:55 +0530

    NinthQuestion

--- frontend/src/questions/ninth-question.jsx
+++ frontend/src/questions/ninth-question.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import {useState} from 'react'
 
 const NinthQuestion = () => {
+
+  const [columns, setColumn] = useState({
+    column1: ['item1', 'item2'],
+    column2: ['item3', 'item4']
+  })
+
   return (
-    <div>NinthQuestion</div>
+
+    <div className='container'>
+      {Object.keys(columns).map((column) => (
+        <div
+          className='column'
+          key={column}
+        >
+          
+        </div>
+      ))}
+    </div>
   )
 }
 
