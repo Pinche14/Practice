f17017546e2ad4b67257893b0052418dafd56c82
Pinche14 <shubham70254@gmail.com>
2024-10-29 23:54:10 +0530

    passing state to route

--- frontend/src/pages/BlogPostPage.jsx
+++ frontend/src/pages/BlogPostPage.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect ,useState} from 'react'
 import { useLocation } from 'react-router-dom'
 
 const BlogPostPage = () => {
-  const routerLocation = useLocation()
+  const [posts, setPosts] = useState([])
+  const {state} = useLocation()
 
-  console.log(routerLocation)
+  useEffect(() => {
+    if(state && state.posts){
+      setPosts(state.posts)
+    }
+  })
+  
+  //console.log(routerLocation)
   return (
     <div>
       <h1>BlogPostPage</h1>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <section>
+            <p>{post.content}</p>
+          </section>
+        </div>
+      ))}
     </div>
   )
 }
