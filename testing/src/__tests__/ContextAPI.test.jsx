7d4fdd9f44ee8508b16593bcccb79678f091eaa4
Pinche14 <shubham70254@gmail.com>
2024-11-23 17:13:40 +0530

    stimulating click events

--- testing/src/__tests__/ContextAPI.test.jsx
+++ testing/src/__tests__/ContextAPI.test.jsx
@@ -1,9 +1,28 @@
 import {expect, it, describe} from 'vitest'
 import {render,screen} from "@testing-library/react"
 import ContextAPI from '../components/ContextAPI'
+import userEvent from '@testing-library/user-event'
 
 describe('ContextAPI', () => {
     it('should render users', () => {
-        render(<ContextAPI/>);
+        const {container} = render(<ContextAPI/>);
+        expect(container).toMatchSnapshot();
+    })
+
+
+    describe('Edit button' , () => {
+        it('should render save button',async () => {
+            render(<ContextAPI/>)
+            const editButton = screen.getByRole('button', {name: 'Edit'});
+            await userEvent.click(editButton)
+            const saveButton = screen.getByRole('button', {name: "Save"});
+            expect(saveButton).toBeInTheDocument();
+        })
+        it('should display username & email input fields when button is clicked',async () => {
+            render(<ContextAPI/>)
+            const render = screen.getByRole('button', {name: "Edit"});
+            await userEvent.click(editButton)
+        })
+    
     })
 })
