e3e7d50d8110cb032768a052a277401be208159a
Pinche14 <shubham70254@gmail.com>
2024-12-25 23:52:34 +0530

    Issue resolved

--- app/client/src/main.jsx
+++ app/client/src/main.jsx
@@ -4,16 +4,15 @@ import { createRoot } from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './styles/index.css'
 import App from './App'
-import Main from './components/Main'
-import Video from './pages/Video'
-import Home from './pages/Home'
-import Photos from './pages/Photos'
-import Header from './components/Header'
-import Footer from './components/Footer'
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App/>
-  </StrictMode>,
+
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+
+root.render(
+    <StrictMode>
+      <App/>
+    </StrictMode>
 )
+
