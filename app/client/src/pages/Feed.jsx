057153b6ffc2a1d1d40e11df2337a0ff1e22a734
Pinche14 <shubham70254@gmail.com>
2025-01-07 23:49:27 +0530

    Video Player

--- app/client/src/pages/Feed.jsx
+++ app/client/src/pages/Feed.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-
+import {Link} from "react-router-dom"
 const Feed = () => {
   return (
     
     <div className='feeds'>
-      <div className='card'>
+      <Link to={'/video/20/4521'} className='card'>
         <img src='' alt="" />
         <h2>Best Channel to learn history</h2>
         <h3>HistoryBuff</h3>
         <p>15k views &bull; 2 days ago</p>
-      </div>
+      </Link>
       <div className='card'>
         <img src='' alt="" />
         <h2>Best Channel to learn history</h2>
--- app/client/src/pages/Video.jsx
+++ app/client/src/pages/Video.jsx
@@ -1,37 +1,41 @@
 import React,{useState,useEffect} from 'react'
+import PlayVideo from './PlayVideo'
 
 const Video = () => {
-    const API_URL = "https://api.sampleapis.com/movies/animation"
+  //   const API_URL = "https://api.sampleapis.com/movies/animation"
 
-  const [videos, setVideos] = useState([])
+  // const [videos, setVideos] = useState([])
 
-  const getVideos = async () => {
-    try {
-      const res = await fetch(API_URL)
-      const json = await res.json()
-      console.log(json)
-      setVideos(json.slice(0,3))
-    } catch (error) {
-      console.error("Error",error)
+  // const getVideos = async () => {
+  //   try {
+  //     const res = await fetch(API_URL)
+  //     const json = await res.json()
+  //     console.log(json)
+  //     setVideos(json.slice(0,3))
+  //   } catch (error) {
+  //     console.error("Error",error)
       
-    }
-  }
+  //   }
+  // }
   
-  useEffect(() => {
-    getVideos(API_URL)
-  },[])
+  // useEffect(() => {
+  //   getVideos(API_URL)
+  // },[])
   return (
-    <div >
-      {videos.map((video,index) => (
-        <div className="photos" key={index}>
-          <video 
-              src={video.posterURL} 
-              controls
-              style={{ width: "100%", maxWidth: "500px" }}
-          />
-          <span>{video.title}</span>
-        </div>
-      ))}
+    // <div >
+    //   {videos.map((video,index) => (
+    //     <div className="photos" key={index}>
+    //       <video 
+    //           src={video.posterURL} 
+    //           controls
+    //           style={{ width: "100%", maxWidth: "500px" }}
+    //       />
+    //       <span>{video.title}</span>
+    //     </div>
+    //   ))}
+    // </div>
+    <div className='play-container'>
+      <PlayVideo/>
     </div>
   )
 }
